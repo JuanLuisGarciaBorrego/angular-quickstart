2c5d325679f8f0dfde0b23e8e2a16b3ab190ebf4
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-10-23 11:39:15 +0200

    mi first angular app

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Import my components
+import { HolaMundoComponent } from './component/hola-mundo.component';
+
 @NgModule({
-  imports:      [ BrowserModule ]
+  imports:      [ BrowserModule ],
+  declarations: [ HolaMundoComponent ],
+  bootstrap: [ HolaMundoComponent ]
 })
 export class AppModule { }
